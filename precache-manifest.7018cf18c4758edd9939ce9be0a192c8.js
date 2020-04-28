5c66c06229b8906bd3e501f656c1386c9563ca03
demi zhao <demizhao@demis-MacBook-Pro.local>
2020-04-28 13:38:57 -0400

    Updates

---
+++ precache-manifest.7018cf18c4758edd9939ce9be0a192c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7584eae0735034d0824c6ce6b87a2fa5",
+    "url": "/demiZhao/legacy-app-to-react.git/index.html"
+  },
+  {
+    "revision": "2dce94a88b2c85d43728",
+    "url": "/demiZhao/legacy-app-to-react.git/static/css/main.e4d93510.chunk.css"
+  },
+  {
+    "revision": "a77bc4fdafca45200e19",
+    "url": "/demiZhao/legacy-app-to-react.git/static/js/2.a00c228b.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/demiZhao/legacy-app-to-react.git/static/js/2.a00c228b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dce94a88b2c85d43728",
+    "url": "/demiZhao/legacy-app-to-react.git/static/js/main.19616103.chunk.js"
+  },
+  {
+    "revision": "aa4424dc64d5d4d295eb",
+    "url": "/demiZhao/legacy-app-to-react.git/static/js/runtime-main.3a969b7d.js"
+  }
+]);
