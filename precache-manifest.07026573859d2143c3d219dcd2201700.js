619fe138fc416b5e7e99fd8715e230c7e34a1bb4
demi zhao <demizhao@demis-MacBook-Pro.local>
2020-04-28 15:31:15 -0400

    Updates

--- precache-manifest.07026573859d2143c3d219dcd2201700.js
+++ precache-manifest.07026573859d2143c3d219dcd2201700.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "017a0b7833217d976d4db7880939444a",
+    "revision": "935c8ec4b0d6d542115d9db50693f4ff",
     "url": "/legacy-app-to-react/index.html"
   },
   {
-    "revision": "211d9d160b3037fb383f",
+    "revision": "9e004711a3eeacf7054c",
     "url": "/legacy-app-to-react/static/css/main.e4d93510.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/legacy-app-to-react/static/js/2.908a0b4d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "211d9d160b3037fb383f",
-    "url": "/legacy-app-to-react/static/js/main.fefaaf2c.chunk.js"
+    "revision": "9e004711a3eeacf7054c",
+    "url": "/legacy-app-to-react/static/js/main.0eff74ad.chunk.js"
   },
   {
     "revision": "c813255f8151d64e7898",
